4011d0e2aa5ce8f7ef84a17b4c4b26ca268dedba
Bolu <boluelbot@gmail.com>
2024-01-11 20:37:43 +0100

    Add license information to source code

--- index.js
+++ index.js
@@ -1,3 +1,20 @@
+/*
+ *  Rere - Discord bot for retrieving information about message reactions
+ *  Copyright (C) 2023-2024  Manuel de Castro <manuel@infor.uva.es>
+ *
+ *  This program is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
 const Discord = require("discord.js");
 const fs = require("fs");
 const { performance } = require("perf_hooks");
